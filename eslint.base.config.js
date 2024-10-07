ad5cdb29c8ae142e19f227daeb2f69cad9f48e86
Bruno <sonego.bruno@gmail.com>
2024-10-07 19:49:32 -0300

    chore: add no console rule

--- eslint.base.config.js
+++ eslint.base.config.js
@@ -7,6 +7,7 @@ module.exports = [
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
+      "no-console": ["error"],
       '@nx/enforce-module-boundaries': [
         'error',
         {
