eedb3c8e54009f8a43e3bf8f1b59eb56c64e5b5e
Bruno <sonego.bruno@gmail.com>
2024-10-20 18:24:16 -0300

    feat(product-search): integrate api

--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,9 +1,11 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { mockProducts } from '@ecommerce/product-data-access';
+import { ProductSearchService } from '@ecommerce/product-data-access';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
 
 @Component({
   selector: 'ecommerce-product-search',
@@ -13,12 +15,20 @@ import { mockProducts } from '@ecommerce/product-data-access';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    AsyncPipe,
   ],
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
 })
 export class ProductSearchComponent {
-  control = new FormControl('');
+  control = new FormControl('', { nonNullable: true });
 
-  products = mockProducts;
+  products$ = this.control.valueChanges.pipe(
+    debounceTime(500),
+    distinctUntilChanged(),
+    filter((text) => text?.length > 1),
+    switchMap((text) => this.productSearchService.searchByName(text))
+  );
+
+  constructor(private productSearchService: ProductSearchService) {}
 }
