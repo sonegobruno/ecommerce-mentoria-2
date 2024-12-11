45ffb353e9f27ff24070e98137569fd12bdc3709
Bruno <sonego.bruno@gmail.com>
2024-12-11 19:55:05 -0300

    fea: add interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,16 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { environment } from '..//environments/environment.local';
 import { ENVIRONMENT } from '..//environments/environment.token';
 import { appRoutes } from './app.routes';
-import { httpErrorsInterceptor } from './interceptors/http.interceptor';
+import { httpErrorsInterceptor } from './interceptors/http-errors/http-errors.interceptor';
+import { httpInterceptor } from './interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes, withComponentInputBinding()),
-    provideHttpClient(withInterceptors([httpErrorsInterceptor])),
+    provideHttpClient(
+      withInterceptors([httpInterceptor, httpErrorsInterceptor])
+    ),
     provideAnimationsAsync(),
     { provide: ENVIRONMENT, useValue: environment },
   ],
--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ENVIRONMENT } from '../../environments/environment.token';
 
-export const httpErrorsInterceptor: HttpInterceptorFn = (req, next) => {
+export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   const environment = inject(ENVIRONMENT);
 
   const clonedRequest = req.clone({
