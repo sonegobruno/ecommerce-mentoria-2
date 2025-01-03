8de54b0671aa5681b9d5db8ff3c18d77fd68668f
Bruno <bsonego@bionexo.com>
2025-01-03 12:01:20 -0300

    test

--- modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthFormComponent } from '../auth-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ecommerce-auth-form-email',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule, ReactiveFormsModule, RouterModule],
   templateUrl: './auth-form-email.component.html',
   styleUrl: './auth-form-email.component.scss',
 })
--- modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthFormComponent } from '../auth-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ecommerce-auth-form-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './auth-form-password.component.html',
   styleUrl: './auth-form-password.component.scss',
 })
